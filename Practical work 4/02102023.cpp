af8a3432ae9292f4ccabaa7a1af46a881d65755c
KevY5 <76757946+KEV0143@users.noreply.github.com>
2023-10-19 22:52:54 +0300

    Add files via upload

---
+++ Practical work 4/02102023.cpp
@@ -0,0 +1,477 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <map>
+using namespace std;
+
+void Number_1() 
+{
+    int i = 0; 
+    double sum = 0;
+    double digital = 0;
+
+    ofstream file("1.txt");
+    while (i != 10) 
+    { 
+        i += 1;
+        cout << i << ") ";
+        if (cin >> digital) 
+        { 
+            file << digital << "\n";
+        }
+        else {
+            cout << "Некорректный ввод" << endl;
+        }
+    }
+    file.close();
+
+    ifstream ifile("1.txt");
+    i = 0;
+    while (i != 10) 
+    { 
+        ifile >> digital;
+        sum += digital;
+        i += 1;
+    }
+    ifile.close();
+
+    cout << "Сумма чисел из файла = " << sum << endl;
+}
+
+int A(float x) 
+{
+    if (x > 0) 
+    {
+        return 1;
+    }
+    else if (x == 0) 
+    {
+        return 0;
+    }
+    return -1;
+}
+
+void Number_2() 
+{
+    double x = 0;
+    while (true) 
+    {
+        cout << "x = ";
+        if (cin >> x) 
+        {
+            cout << A(x) << endl;
+        }
+        else 
+        {
+            cout << "Некорректный ввод";
+            break;
+        }
+    }
+}
+
+double pr (double a, double b)  // прямоугольник
+{ 
+    if (a <= 0 or b <= 0) 
+    {
+        return -1;
+    }
+    return a * b;
+}
+
+double tr (double a, double b, double c) // треугольник
+{ 
+    if (a <= 0 or b <= 0 or c <= 0 or a + b <= c or a + c <= b or b + c <= a) 
+    {
+        return -1;
+    }
+    double p = (a + b + c) / 2;
+    return sqrt(p * (p - a) * (p - b) * (p - c));
+}
+
+double kr (double r)  // круг
+{ 
+    if (r <= 0) 
+    {
+        return -1;
+    }
+    return 3.14 * pow(r, 2);
+}
+
+void Number_3() 
+{
+    int select;
+    double a, b, c, r;
+    cout << "Выберите: 1 ( Прямоугольник ) / 2 ( Треугольник ) / 3 ( Круг ): ";
+    cin >> select;
+    switch (select) 
+    {
+
+    case 1: // прямоугольник
+        cout << "Введите сторону a = ";
+        cin >> a;
+        cout << "Введите сторону b = ";
+        cin >> b;
+        if (pr(a, b) == -1) 
+        {
+            cout << "Некорректный ввод";
+        }
+        else 
+        {
+            cout << "Площадь прямоугольника = " << pr(a, b) << endl;
+        }
+        break;
+
+    case 2: // треугольник
+        cout << "Введите сторону a = ";
+        cin >> a;
+        cout << "Введите сторону b = ";
+        cin >> b;
+        cout << "Введите сторону c = ";
+        cin >> c;
+        if (tr(a, b, c) == -1) 
+        {
+            cout << "Некорректный ввод";
+        }
+        else 
+        {
+            cout << "Площадь треугольника = " << tr(a, b, c) << endl;
+        }
+        break;
+    
+    case 3: // круг
+        cout << "Введите радиус круга = ";
+        cin >> r;
+        if (kr(r) == -1) 
+        {
+            cout << "Некорректный ввод";
+        }
+        else 
+        {
+            cout << "Площадь круга = " << kr(r) << endl;
+        }
+        break;
+    default:
+        cout << "Некорректный ввод";
+    }
+}
+
+void Number_4()
+{
+    for (int i = 0; i < 8; i++) 
+    {
+        cout << "  *  *  *  *  *  *";
+        cout << "_____________________" << endl;
+    }
+
+    for (int i = 0; i < 5; i++) 
+    { 
+        cout << "_______________________________________" << endl;
+    }
+}
+
+void Number_5()
+{
+    const int width = 80; // ширина графика
+    const int height = 30; // высота графика
+    const double xMin = -3.14; // минимальное значение x
+    const double xMax = 3.14; // максимальное значение x
+    const double yMin = -1.0; // минимальное значение y
+    const double yMax = 1.0; // максимальное значение y
+
+    for (int i = 0; i < height; i++) 
+    {
+        double y = (double)i / height * (yMax - yMin) + yMin;
+        for (int j = 0; j < width; j++) 
+        {
+            double x = (double)j / width * (xMax - xMin) + xMin;
+            double sinX = tan(x);
+            
+            if (abs(sinX - y) < 0.05) 
+            {
+                cout << "*";
+            }
+            else if (i == height / 2 && j == width / 2) 
+            {
+                cout << "+";
+            }
+            else if (i == height / 2) 
+            {
+                cout << "-";
+            }
+            else if (j == width / 2) 
+            {
+                cout << "|";
+            }
+            else 
+            {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+}
+
+
+void Number_6() 
+{
+    string directory = "C:/Users/KEV/Desktop/MIREA_Jobs-main/02102023/6.txt";
+    string filename = "6.txt";
+
+    cout << "Введите римскую цифру: ";
+    string number;
+    cin >> number;
+
+    ifstream in(directory);
+    if (in.is_open()) 
+    {
+        string line;
+        while (getline(in, line)) 
+        {
+            string firstPart = "";
+            string NUM = "";
+            bool whasEq = false;
+            bool secEq = false;
+
+            for (char c : line) {
+                if (c == '=') {
+                    if (!whasEq) {
+                        whasEq = true;
+                    }
+                    else {
+                        secEq = true;
+                        break;
+                    }
+                    continue;
+                }
+                if (whasEq) {
+                    NUM += c;
+                }
+                else {
+                    firstPart += c;
+                }
+            }
+
+            if (number == NUM) {
+                cout << firstPart << endl;
+                return;
+            }
+        }
+        cout << "Некорректный ввод, ошибка." << endl;
+    }
+    else {
+        cout << "Ошибка дериктории." << endl;
+    }
+}
+
+int random_number(int SI, int m, int i, int c) 
+{
+    return SI = (m * SI + i) % c;
+}
+
+void Number_7() 
+{
+    int SI = 1;
+    int m, i, c;
+    int variant;
+    cout << "Выбирите вариант (1 или 2): ";
+    cin >> variant;
+
+    if (variant == 1) 
+    {
+        m = 37;
+        i = 3;
+        c = 64;
+    }
+    else if (variant == 2) 
+    {
+        m = 25173;
+        i = 13849;
+        c = 65537;
+    }
+    else 
+    {
+        cout << "Неправильный ввод" << endl;
+    }
+
+    for (int j = 0; j < 10; j++) 
+    {
+        cout << SI << endl;
+        SI = random_number(SI, m, i, c);
+    }
+}
+
+void Number_8()
+{
+    double a[3][4] =
+    {
+        {5, 2, 0, 10},
+        {3, 5, 2, 5},
+        {20, 0, 0, 0} };
+
+    double b[4][2] =
+    {
+        {1.20, 0.50},
+        {2.80, 0.40},
+        {5.00, 1.00},
+        {2.00, 1.50} };
+
+    double c[3][2] = {};
+
+    for (int i = 0; i < 3; i++) 
+    {
+        for (int j = 0; j < 2; j++) 
+        {
+            for (int k = 0; k < 4; k++) 
+            {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+        }
+    }
+    cout << endl;
+    int maxSeller = 0;
+    int minSeller = 0;
+    double maxMoney = -1e9;
+    double minMoney = 1e9;
+    for (int i = 0; i < 3; i++) 
+    {
+        if (c[i][0] > maxMoney) 
+        {
+            maxSeller = i + 1;
+            maxMoney = c[i][0];
+        }
+        if (c[i][0] < minMoney) 
+        {
+            minSeller = i + 1;
+            minMoney = c[i][0];
+        }
+    }
+    cout << " | Продавец " << maxSeller << " | Получил максимум денег: " << maxMoney << endl;
+    cout << " | Продавец " << minSeller << " | Получил минимум денег: " << minMoney << endl;
+
+    maxSeller = 0;
+    minSeller = 0;
+    double maxComss = -1e9;
+    double minComss = 1e9;
+    for (int i = 0; i < 3; i++) 
+    {
+        if (c[i][1] > maxComss) 
+        {
+            maxSeller = i + 1;
+            maxComss = c[i][1];
+        }
+        if (c[i][1] < minComss) 
+        {
+            minSeller = i + 1;
+            minComss = c[i][1];
+        }
+    }
+    cout << " | Продавец " << maxSeller << " | Получил наибольшие комиссионные: " << maxComss << endl;
+    cout << " | Продавец " << minSeller << " | Получил наименьшие комиссионные: " << minComss << endl;
+    cout << endl;
+    double sumMoney = 0.0;
+    for (int i = 0; i < 3; i++) 
+    {
+        sumMoney += c[i][0];
+    }
+    cout << " | Общая сумма денег вырученная за проданные товары: " << sumMoney << endl;
+
+    double sumComss = 0.0;
+    for (int i = 0; i < 3; i++) 
+    {
+        sumComss += c[i][1];
+    }
+    cout << " | Всего комиссионных получили продавцы: " << sumComss << endl;
+
+    double SumMoneyResult = 0.0;
+    for (int i = 0; i < 3; i++)
+    {
+        SumMoneyResult += c[i][0] + c[i][1];
+    }
+    cout << " | Общая сумма денег прошедшая через руки продавцов: " << SumMoneyResult << endl;
+}
+
+int cI(char c) {
+    if (c >= '0' && c <= '9') {
+        return c - '0';
+    }
+    else {
+        return c - 'A' + 10;
+    }
+}
+
+char iC(int n) {
+    if (n >= 0 && n <= 9) {
+        return n + '0';
+    }
+    else {
+        return n - 10 + 'A';
+    }
+}
+
+string osnov(string num, int oldosn, int newosn) {
+    if (oldosn < 2 || oldosn > 16 || newosn < 2 || newosn > 16) {
+        return "Основание должно быть в диапазоне от 2 до 16";
+    }
+    for (int i = 0; i < num.size(); i++) {
+        if (cI(num[i]) >= oldosn) {
+            return "Неправильное введённое число для заданного основания";
+        }
+    }
+    string result = "";
+    int decimalNum = 0;
+    int power = 1;
+    for (int i = num.size() - 1; i >= 0; i--)
+    {
+        int digit = cI(num[i]);
+        decimalNum += digit * power;
+        power *= oldosn;
+    }
+    while (decimalNum > 0)
+    {
+        int digit = decimalNum % newosn;
+        result = iC(digit) + result;
+        decimalNum /= newosn;
+    }
+    return result;
+}
+
+void Number_9() {
+    string num;
+    int oldosn, newosn;
+    cout << "Введите число: ";
+    cin >> num;
+    cout << "Введите старое основание: ";
+    cin >> oldosn;
+    cout << "Введите новое основание: ";
+    cin >> newosn;
+    if (oldosn < 2 || oldosn > 36 || newosn < 2 || newosn > 36) {
+        cout << "Основание должно быть в диапазоне от 2 до 36" << endl;
+        return;
+    }
+    string result = osnov(num, oldosn, newosn);
+    cout << "Результат: " << result << endl;
+}
+
+int main()
+{
+    setlocale(0, "Russian");
+    //Number_1();
+    //Number_2();
+    //Number_3();
+    //Number_4();
+    //Number_5();
+    Number_6();
+    //Number_7();
+    //Number_8();
+    //Number_9();
+}
---
+++ Practical work 5/09102023.cpp
@@ -0,0 +1,418 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+#include <map>
+#include <cmath>
+#include <algorithm>
+#include <string.h>
+#include <string>
+#include <cctype>
+#include <sstream>
+
+using namespace std;
+
+int euclideanAlgorithm(int a, int b)
+{
+    int remainder;
+    while (b != 0)
+    {
+        remainder = a % b;
+        a = b;
+        b = remainder;
+    }
+    return a;
+}
+
+void Number_1()
+{
+    int a, b;
+    cout << "Введите два целых числа: ";
+
+    while (!(cin >> a >> b) || a == 0 || b == 0)
+    {
+        cout << "Неверный ввод. Попробуйте снова: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int choice;
+    cout << "Выберите алгоритм Евклида (1 - делением, 2 - вычитанием): ";
+
+    while (!(cin >> choice))
+    {
+        cout << "Неверный ввод. Попробуйте снова: ";
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int gcd;
+    switch (choice)
+    {
+    case 1:
+        gcd = euclideanAlgorithm(a, b);
+        break;
+    case 2:
+        if (a < 0)
+            a = -a;
+        if (b < 0)
+            b = -b;
+
+        while (a != b)
+        {
+            if (a > b)
+            {
+                a -= b;
+            }
+            else {
+                b -= a;
+            }
+        }
+        gcd = a;
+        break;
+    default:
+        cout << "Неверный выбор алгоритма.";
+    }
+
+    cout << "Наибольший общий делитель: " << gcd << endl;
+}
+
+void ERA(int n)
+{
+    vector <bool> prime(n + 1, true);
+
+    prime[0] = false;
+    prime[1] = false;
+
+    for (int p = 2; p * p <= n; p++)
+    {
+        if (prime[p] == true)
+        {
+            for (int i = p * p; i <= n; i += p)
+            {
+                prime[i] = false;
+            }
+        }
+    }
+
+    cout << "Простые числа до " << n << ": ";
+    for (int i = 2; i <= n; i++)
+    {
+        if (prime[i])
+        {
+            cout << i << " ";
+        }
+    }
+}
+
+void Number_2()
+{
+    double n;
+    cout << "Введите натуральное число: ";
+    cin >> n;
+
+    while (n <= 1 || floor(n) != n)
+    {
+        cin.clear();
+        cin.ignore(numeric_limits <streamsize>::max(), '\n');
+        if (n == 1)
+        {
+            cout << "Ошибка! Введите число больше 1: ";
+        }
+        else
+        {
+            cout << "Ошибка! Введите корректное натуральное число: ";
+        }
+        cin >> n;
+    }
+
+    ERA(static_cast<int>(n));
+}
+
+void Number_31() 
+{
+    ofstream file("31.txt");
+    if (file.is_open()) {
+        string text;
+        cout << "Введите текст: ";
+        getline(cin, text);
+        file << text;
+        file.close();
+    }
+
+    ifstream infile("31.txt");
+    int consonants[26] = {};
+    char c;
+    bool foundConsonants = false;
+    int maxCount = 0;
+    char maxConsonant;
+
+    while (infile >> c) 
+    {
+        if (isalpha(c) && !strchr("AEIOUaeiou", c)) 
+        {
+            consonants[toupper(c) - 'A']++;
+            foundConsonants = true; 
+        }
+    }
+    if (!foundConsonants) 
+    { 
+        cout << "Ошибка: в тексте не найдено согласных букв." << endl;
+    }
+
+    for (int i = 0; i < 26; i++) 
+    {
+        if (consonants[i] > maxCount) 
+        {
+            maxCount = consonants[i];
+            maxConsonant = 'A' + i;
+        }
+    }
+    cout << "Наиболее часто встречающаяся согласная буква: " << maxConsonant << endl;
+
+}
+
+void Number_32()
+{
+    ofstream file("32.txt");
+    if (file.is_open()) {
+        string text;
+        cout << "Введите текст: ";
+        getline(cin, text);
+        file << text;
+        file.close();
+    }
+
+    else 
+    {
+        cerr << "Ошибка открытия файла" << endl;
+    }
+
+    string filename;
+    cout << "Введите имя файла: ";
+    cin >> filename;
+
+    ifstream file2(filename);
+    if (!file2.is_open()) 
+    {
+        cerr << "Ошибка открытия файла" << endl;
+    }
+
+    map <char, int> freq;
+
+    char c;
+    while (file2.get(c)) {
+        if (freq.find(c) == freq.end()) {
+            freq[c] = 1;
+        }
+        else {
+            freq[c]++;
+        }
+    }
+
+    for (auto& pair : freq) {
+        cout << "Символ '" << pair.first << "' встречается " << pair.second << " раз." << endl;
+    }
+}
+
+string trans(string num, int startBase, int resultBase)
+{
+    const string digits = "0123456789ABCDEF";
+    if (resultBase <= 1 || resultBase > 16) 
+    {
+        cout << "Ошибка";
+        return "Ошибка";
+    }
+
+    int counter = 0;
+    int result = 0;
+    for (char c : num) {
+        int charNum = digits.find(c);
+        if (charNum >= startBase) {
+            cout << "Ошибка";
+            return "Ошибка";
+        }
+        result += charNum * pow(startBase, counter);
+        counter++;
+    }
+
+    string final = "";
+    while (result != 0) {
+        int div = result % resultBase;
+        result /= resultBase;
+        final = digits[div] + final;
+    }
+    return final;
+}
+
+void Number_41() 
+{
+    cout << "Длина массива: ";
+    int len;
+    cin >> len;
+    if (len <= 0) 
+    {
+        cout << "Ошибка"; return;
+    }
+    vector<string> vect(len);
+    for (int i = 0; i < vect.size(); i++) {
+        string a;
+        cout << "Введите число с основанием 3: ";
+        cin >> a;
+        vect[i] = trans(a, 3, 6);
+    }
+    cout << "Массив равен: " << endl;
+    for (int i = 0; i < vect.size(); i++) {
+        cout << vect[i] << endl;
+    }
+}
+
+int count_negative_triplets(vector<int> c, int n) {
+    int count = 0;
+    for (int i = 0; i < n - 2; i++) {
+        int neg = 0; 
+        for (int j = i; j < i + 3; j++) { 
+            if (c[j] < 0) {
+                neg++;
+            }
+        }
+        if (neg == 3) { 
+            count++;
+        }
+    }
+    return count;
+}
+
+void Number_42() 
+{
+    vector<int> c(10);
+    cout << "Введите число: ";
+
+    for (int i2 = 0; i2 < 10; i2++) {
+        cin >> c[i2];
+    }
+    int result = count_negative_triplets(c, 10);
+    cout << "Количество троек отрицательных чисел: " << result << endl;
+}
+void Number_51()
+{
+    int kol, z, s, b;
+    string c;
+    vector <string> str(0);
+    vector <int> zol(0);
+    vector <int> ser(0);
+    vector <int> bro(0);
+    vector <int> vse(0);
+    vector <int> bal(0);
+    cout << "Введите колличество стран-участниц: ";
+    cin >> kol;
+    cout << "Введите названия стран: " << endl;
+    for (int i = 0; i < kol; i++)
+    {
+        cin >> c;
+        str.push_back(c);
+    }
+
+    sort(str.begin(), str.end());
+
+    cout << "Введите колличество золотых, серебрянных, бронзовых медалей у: " << endl;
+    for (int i = 0; i < kol; i++) 
+    {
+        cout << str[i] << ": ";
+        cin >> z >> s >> b;
+        zol.push_back(z);
+        ser.push_back(s);
+        bro.push_back(b);
+        vse.push_back(z + s + b);
+        bal.push_back(7 * z + 6 * s + 5 * b);
+
+    }
+
+    ofstream fout;
+    fout.open("51.txt");
+    for (int i = 0; i < kol; i++) {
+        fout << str[i] << " " << zol[i] << " " << ser[i] << " " << bro[i] << " " << vse[i] << " " << bal[i];
+        fout << endl;
+    }
+    ifstream ff2("51.txt");
+    char buff[10000];
+    if (ff2.is_open()) 
+    {
+        ff2.getline(buff, 10000);
+        ff2.close();
+        cout << buff << endl;
+    }
+}
+
+struct Student 
+{
+    string id;
+    string name;
+    vector<pair<string, int>> subjects; 
+};
+
+
+bool compareStudents(const Student& s1, const Student& s2) 
+{
+    return s1.name < s2.name;
+}
+
+void Number_52() {
+    ofstream file("52.txt"); 
+
+    file << "12345 Климкин Иван Иванович Математика 5 Физика 4 Химия 3 Литература 5 История 4" << endl;
+    file << "23456 Рысин Петр Петрович Математика 4 Физика 5 Химия 4 Литература 3 История 5" << endl;
+    file << "34567 Дудин Алексей Владимирович Математика 3 Физика 4 Химия 5 Литература 5 История 4" << endl;
+    file << "34567 Агеев Алексей Владимирович Математика 3 Физика 4 Химия 5 Литература 5 История 4" << endl;
+
+    file.close(); 
+
+    ifstream inputFile("52.txt");
+    vector<Student> students; 
+
+
+    string line;
+    while (getline(inputFile, line)) {
+        istringstream iss(line);
+        Student student;
+        iss >> student.id >> student.name;
+
+        string subject;
+        int grade;
+        while (iss >> subject >> grade) {
+            student.subjects.push_back(make_pair(subject, grade));
+        }
+
+        students.push_back(student);
+    }
+
+    inputFile.close();
+
+    sort(students.begin(), students.end(), compareStudents);
+
+    for (const auto& student : students) {
+        cout << "Номер зачетной книжки: " << student.id << endl;
+        cout << "ФИО студента: " << student.name << endl;
+        cout << "Предметы и оценки: ";
+        for (const auto& subject : student.subjects) {
+            cout << subject.first << " " << subject.second << " ";
+        }
+        cout << endl << endl;
+    }
+
+}
+
+
+
+int main()
+{
+    setlocale(0, "Russian");
+   // Number_1();
+   // Number_2();
+   // Number_31();
+   // Number_32();
+   // Number_41();
+   // Number_42();
+   // Number_51();
+   Number_52();
+
+}
